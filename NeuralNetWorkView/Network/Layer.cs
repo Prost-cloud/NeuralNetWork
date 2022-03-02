545e17668f4654e476aa2a89e70e9008234313a5
влад <vvladik9798@gmail.com>
2022-03-02 18:12:24 +0300

    Adding network classes

---
+++ NeuralNetWorkView/Network/Layer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NeuralNetwork.Network
+{
+    public class Layer
+    {
+        public int Size { get; private set; }
+        public double[] Neurons { get; set; }
+        public double[] Biases { get; set; }
+        public double[,] Weights { get; set; }
+
+        public Layer(int size, int nextSize)
+        {
+            Size = size;    
+            Neurons = new double[size];
+            Biases = new double[size];
+            Weights = new double[size, nextSize];
+        }
+
+    }
+}
